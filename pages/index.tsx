5a4fac4f1c27c345ed15607a0cb608efc196046f
Michelle Dang <michelle.dang3929@gmail.com>
2021-02-21 18:24:54 -0500

    add standardised-audio-context npm package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,46 @@
 import styled from 'styled-components';
+import { AudioContext } from 'standardized-audio-context';
+
+export default function Home() {
+  const generateSineWave = () => {
+    const audioContext = new AudioContext();
+    const oscillatorNode = audioContext.createOscillator();
+
+    oscillatorNode.connect(audioContext.destination);
+    oscillatorNode.start();
+  };
+
+  const exampleSetup = () => {
+    var stream = new Audio('/audio/file1.mp3');
+    stream.play();
+
+    const audioContext = new AudioContext();
+    var analyser = audioContext.createAnalyser();
+
+    var source = audioContext.createMediaElementSource(stream);
+    source.connect(analyser);
+    analyser.connect(audioContext.destination);
+
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+
+    analyser.getByteTimeDomainData(dataArray);
+    console.log(dataArray);
+  };
+  return (
+    <Wrapper>
+      <Title>Visualizer</Title>
+      <StyledButton onClick={generateSineWave}>sine wave</StyledButton>
+      <StyledButton onClick={exampleSetup}>example setup</StyledButton>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Title = styled.h1`
   font-size: 50px;
@@ -6,6 +48,9 @@ const Title = styled.h1`
   color: ${({ theme }) => theme.colors.primary};
 `;
 
-export default function Home() {
-  return <Title>Visualizer</Title>;
-}
+const StyledButton = styled.button`
+  display: flex;
+  width: fit-content;
+  align-self: center;
+  margin: 10px;
+`;
