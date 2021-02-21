c1b0a8b231b5aa5d5f204f0f66d9ad228800aaae
Michelle Dang <michelle.dang3929@gmail.com>
2021-02-21 15:27:04 -0500

    setup with nextjs, styled-components, ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 const Title = styled.h1`
   font-size: 50px;
+  text-align: center;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <Title>Visualizer</Title>;
 }
